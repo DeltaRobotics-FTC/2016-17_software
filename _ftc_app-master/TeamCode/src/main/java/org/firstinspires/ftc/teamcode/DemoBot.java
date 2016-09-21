f1da21f77c35e5e0cda574c02d33075416a18829
NolanEastburn <nolieman1@gmail.com>
2016-09-20 20:54:31 -0500

    Edited the 4-wheel drive program so motors go the right way and the correct sticks control the correct motors -Nolan Eastburn

--- _ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoBot.java
+++ _ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoBot.java
@@ -5,49 +5,59 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 /**
  * Created by RoboticsUser on 9/20/2016.
  */
 @TeleOp (name = "DemoBot", group = "")
 public class DemoBot extends OpMode
+{
+    DcMotor motorLeftFront;
+    DcMotor motorRightFront;
+    DcMotor motorLeftBack;
+    DcMotor motorRightBack;
+
+    float throttleLeft = 0;
+    float throttleRight = 0;
+
+
+    @Override
+    public void init()
     {
-        DcMotor motorLeftFront;
-        DcMotor motorRightFront;
-        DcMotor motorLeftBack;
-        DcMotor motorRightBack;
+        motorLeftFront = hardwareMap.dcMotor.get("motorLeftFront");
+        motorRightFront = hardwareMap.dcMotor.get("motorRightFront");
+        motorLeftBack = hardwareMap.dcMotor.get("motorLeftBack");
+        motorRightBack = hardwareMap.dcMotor.get("motorRightBack");
 
-        float throttleLeft;
-        float throttleRight;
-
-
-        public void init()
-            {
-               hardwareMap.dcMotor.get("motorLeftFront");
-               hardwareMap.dcMotor.get("motorRightFront");
-                hardwareMap.dcMotor.get("motorLeftBack");
-                hardwareMap.dcMotor.get("motorRightBack");
-
-
-            }
-
-
-        public void loop()
-            {
-
-                gamepad1.left_stick_y = throttleLeft;
-                gamepad1.right_stick_y = throttleRight;
-                Range.clip(throttleLeft, -1.0, 1.0);
-                Range.clip(throttleRight, -1.0, 1.0);
-                motorLeftFront.setPower(throttleLeft);
-                motorRightFront.setPower(throttleRight);
-                motorLeftBack.setPower(throttleLeft);
-                motorRightBack.setPower(throttleRight);
-
-
-
-
-
-
-            }
 
     }
+
+
+    @Override
+    public void loop()
+    {
+
+        throttleLeft = Range.clip(throttleLeft, -1, 1);
+        throttleRight = Range.clip(throttleRight, -1, 1);
+        telemetry.addData("Left Stick Y", gamepad1.left_stick_y);
+        telemetry.addData("Right Stick Y", gamepad1.right_stick_y);
+        telemetry.addData("throttleRight", throttleRight);
+        telemetry.addData("throttleLeft", throttleLeft);
+
+        throttleLeft = gamepad1.right_stick_y;
+        throttleRight = gamepad1.left_stick_y;
+
+        motorLeftFront.setPower(throttleLeft);
+        motorRightFront.setPower(-throttleRight);
+        motorLeftBack.setPower(throttleLeft);
+        motorRightBack.setPower(-throttleRight);
+
+
+
+
+
+
+    }
+
+}
