13f8592b1b0bf551807a54d727bff871505dc5d3
LauraAEvans <elzagirl10@gmail.com>
2016-10-29 09:57:39 -0500

    -Implemented ODS Sensor, going to create branch

---
+++ _ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ODS_Test.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+
+ * Created by RoboticsUser on 10/29/2016.
+ */
+@Autonomous (name = "ODS_Test", group = "")
+public class ODS_Test extends OpMode
+    {
+
+        OpticalDistanceSensor ODS;
+
+
+        public void init()
+        {
+            ODS = hardwareMap.opticalDistanceSensor.get("ODS");
+
+
+        }
+
+
+        public void loop()
+        {
+
+            telemetry.addData("ODSRawLightDetected", readAvgODSVal(ODS));
+
+
+        }
+
+
+        public double readAvgODSVal(OpticalDistanceSensor ODS)
+        {
+            double averagedRawLight = 0;
+            for (int i = 0; i < 10; ++i )
+            {
+                averagedRawLight += ODS.getRawLightDetected();;
+            }
+
+            averagedRawLight /= 10;
+
+
+
+            return  averagedRawLight;
+        }
+
+
+    }
